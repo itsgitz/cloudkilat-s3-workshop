a8eee85de66204113f10a77adcc2d288e454425c
itsgitz <anggit.ginanjar@outlook.com>
2021-01-17 15:14:07 +0700

    Add createBucket function. Todo: fix the region bugs.

--- app.js
+++ app.js
@@ -4,45 +4,16 @@
  */
 
 let http = require('http');
+let uuid = require('uuid');
 let config = require('./config/config');
 let aws = require('aws-sdk');
 
 http.createServer(function (req, res) {
-    // show list objects
-    let kilatListObject = new aws.S3({
-        endpoint: config.kilatStorageEndpoint,
-        credentials: {
-            accessKeyId: config.accessKeyId,
-            secretAccessKey: config.secretAccessKey
-        }
-    }).listObjects({
-        Bucket: config.bucketName
-    }).promise();
+    kilatStorageCreateBucket();
+    kilatStorageGetObject();
+    kilatStorageListObjects();
 
-    kilatListObject.then(function(data) {
-        console.log('listObject:', data)
-    }).catch(err => {
-        console.error(err);
-    })
-    // Get Object
-    let kilatGetObject = new aws.S3({
-        endpoint: config.kilatStorageEndpoint,
-        credentials: {
-            accessKeyId: config.accessKeyId,
-            secretAccessKey: config.secretAccessKey
-        }
-    }).getObject({
-        Bucket: config.bucketName, 
-        Key: config.storageObject
-    }).promise();
-
-    kilatGetObject.then(function(data) {
-        console.log('getObject:', data);
-    }).catch(err => {
-        console.error(err);
-    })
-
-    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
     // show config:
     console.log(config);
@@ -56,3 +27,80 @@ http.createServer(function (req, res) {
         </body>
     `);
 }).listen(8000);
+
+
+function kilatStorageCreateBucket() {
+    let newBucket = 'new-bucket-' + uuid.v4();
+    let keyObject = 'hello-itsgitz.txt'; // nama file
+    let s3 = new aws.S3({
+        region: config.region,
+        endpoint: config.kilatStorageEndpoint,
+        credentials: {
+            accessKeyId: config.accessKeyId,
+            secretAccessKey: config.secretAccessKey
+        }
+    }).createBucket({Bucket: newBucket}).promise();
+
+    s3.then(function (data) {
+        console.log(data);
+
+        // // create new object or file
+        // newObject = new aws.S3({
+        //     region: config.region,
+        //     endpoint: config.kilatStorageEndpoint,
+        //     credentials: {
+        //         accessKeyId: config.accessKeyId,
+        //         secretAccessKey: config.secretAccessKey
+        //     }
+        // }).putObject({Bucket: newBucket, Key: keyObject, Body: 'Hello from itsgitz!'}).promise();
+
+        // newObject.then(function(data) {
+        //     console.log(data);
+        // }).catch(err => {
+        //     console.error(err);
+        // });
+
+    }).catch(err => {
+        console.error(err)
+    })
+}
+
+function kilatStorageGetObject() {
+    let s3 = new aws.S3({
+        region: config.region,
+        endpoint: config.kilatStorageEndpoint,
+        credentials: {
+            accessKeyId: config.accessKeyId,
+            secretAccessKey: config.secretAccessKey
+        }
+    }).getObject({
+        Bucket: config.bucketName,
+        Key: config.storageObject
+    }).promise();
+
+    s3.then(function (data) {
+        console.log('getObject:', data);
+    }).catch(err => {
+        console.error(err);
+    })
+}
+
+function kilatStorageListObjects() {
+    // show list objects
+    let s3 = new aws.S3({
+        region: config.region,
+        endpoint: config.kilatStorageEndpoint,
+        credentials: {
+            accessKeyId: config.accessKeyId,
+            secretAccessKey: config.secretAccessKey
+        }
+    }).listObjects({
+        Bucket: config.bucketName
+    }).promise();
+
+    s3.then(function (data) {
+        console.log('listObject:', data)
+    }).catch(err => {
+        console.error(err);
+    })
+}
--- config/config.example.js
+++ config/config.example.js
@@ -3,5 +3,6 @@ const secretAccessKey = '';
 const kilatStorageEndpoint = 'https://s3-id-jkt-1.kilatstorage.id/';
 const bucketName = '';
 const storageObject = '';
+const region = 'us-east-1';
 
-module.exports = { accessKeyId, secretAccessKey, kilatStorageEndpoint, bucketName, storageObject };
+module.exports = { accessKeyId, secretAccessKey, kilatStorageEndpoint, bucketName, storageObject, region };
